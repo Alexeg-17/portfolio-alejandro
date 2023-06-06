2def1f4d0f0d8192b223a6010ba96fce346e5b31
Alexeg-17 <alex.e.g.107@gmail.com>
2023-06-05 23:47:20 -0600

    adding form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { DecimalPipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ThemeServiceService } from './services/theme-service.service';
-import { DecimalPipe } from '@angular/common';
-import { TemplateModule } from './template/template.module';
 import { PagesModule } from './pages/pages.module';
+import { ThemeServiceService } from './services/theme-service.service';
+import { TemplateModule } from './template/template.module';
+
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import * as Notiflix from 'notiflix';
 
 @Component({
   selector: 'app-contact',
@@ -8,8 +10,25 @@ import { Component, OnInit } from '@angular/core';
 export class ContactComponent implements OnInit {
 
   constructor() { }
+  
+
 
   ngOnInit(): void {
   }
-
+  
+  public sendEmail(e: Event) {
+Notiflix.Loading.standard('sending...');
+    e.preventDefault();
+    const formulario = document.getElementById('myForm') as HTMLFormElement;
+ 
+    emailjs.sendForm('service_ft4knx7', 'template_boss66b' ,e.target as HTMLFormElement, 'xXlDT3UP043RxSO4I')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        Notiflix.Loading.remove();
+        formulario.reset();
+      }, (error) => {
+        console.log(error.text);
+        
+      });
+  }
 }
